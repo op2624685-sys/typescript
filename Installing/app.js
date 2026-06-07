f783c357eaee60f6d5128584332cb24de0ed8424
Om Prakash <op2624685@gmail.com>
2026-06-07 22:00:17 +0530

    command to compile in real time

--- Installing/app.js
+++ Installing/app.js
@@ -1,6 +1,9 @@
 "use strict";
-// this file automatically created by tsc(typescript compiler)
+// This file automatically created by tsc(typescript compiler)
 // command -> tsc app.ts
+
+// If you want to compile in real time
+// command -> tsc app.ts -w
 
 var a = 10;
 var b = 20;
