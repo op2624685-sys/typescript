c3c4e84b736d726c42b99e513cf3a7f9a243833c
Om Prakash <op2624685@gmail.com>
2026-06-07 21:43:27 +0530

    typescript installation

---
+++ Installing/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// this file automatically created by tsc(typescript compiler)
+// command -> tsc app.ts
+
+var a = 10;
+var b = 20;
+function add(a, b) {
+    return a + b;
+}
---
+++ Installing/app.ts
@@ -0,0 +1,9 @@
+// install typescript command 
+// -> npm i typescript -g
+
+var a = 10;
+var b = 20;
+
+function add(a: number, b: number): number {
+    return a + b;
+}
