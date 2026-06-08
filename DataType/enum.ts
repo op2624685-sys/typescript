0256a5f1214b0fbcd20d6f2b9952900a70cdefb4
Om Prakash <op2624685@gmail.com>
2026-06-08 22:57:50 +0530

    learn about enum and it's use cases

---
+++ DataType/enum.ts
@@ -0,0 +1,32 @@
+// enum is used to define a set of named constants
+
+enum Model {
+    Honda,
+    Suzuki,
+    Toyota
+}
+
+enum HondaColors {
+    Red,
+    Blue
+}
+
+enum SuzukiColors {
+    Red,
+    White,
+    Blue
+}
+
+enum ToyotaColors {
+    Red,
+    White,
+    Black,
+    Blue
+}
+
+//This will help to access the enum
+Model.Honda;
+HondaColors.Red;
+
+Model.Suzuki;
+SuzukiColors.White;
