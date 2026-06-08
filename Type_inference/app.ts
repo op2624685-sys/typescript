a20b7bd01296f8e22401a7650f7384b61697551e
Om Prakash <op2624685@gmail.com>
2026-06-08 23:47:47 +0530

    learn about type inference and type annotation

---
+++ Type_inference/app.ts
@@ -0,0 +1,11 @@
+// Type Inference
+// It is a feature of typescript that it will automatically infer the type of the variable that will called the type inference
+let a = 10; // here it will automatically infer the type of the variable and set it to number
+let b = "hello"; // here it will automatically infer the type of the variable and set it to string
+let c = true; // same like that it will automatically infer the type of the variable and set it to boolean
+
+
+// Type annotation is used to specify the type of the variable
+let d: number = 10;
+let e: string = "hello";
+let f: boolean = true;
